5b7f6fc5a5a69be1a0feda17a0b1645592c9f7c7
Daniel <daziev24@gmail.com>
2026-05-05 21:05:52 -0400

    Adjust data_loader, created + ran extra credit dataset

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -51,7 +51,7 @@ def load_digits_dataset():
     
     return X, y
 
-def load_fashion_mnist_sample(sample_size=5000):
+def load_fashion_mnist_sample(sample_size=2000):
     """
     Pulls subset of Fashion-MNIST
     Returns X as (784, samples) and y as (1, samples)
